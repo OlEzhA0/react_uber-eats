743718d63cca7f81075c4067f9d84afd96b5e959
Oleg <60295799+OlEzhA0@users.noreply.github.com>
2020-06-18 22:44:16 +0300

    Updates

---
+++ precache-manifest.fed78a0b56d04a755440c79ecf6941a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011e4b82a39a2f5e2deeb08d44ec4c90",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "5223c480660aae03d97f",
+    "url": "/react_uber-eats/static/css/main.78238c4b.chunk.css"
+  },
+  {
+    "revision": "bcb718e9cda073a663da",
+    "url": "/react_uber-eats/static/js/2.fe088a03.chunk.js"
+  },
+  {
+    "revision": "5223c480660aae03d97f",
+    "url": "/react_uber-eats/static/js/main.9679481b.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
